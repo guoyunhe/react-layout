415bc3f7caceda1d4c5f6ef9e972f5a99ca92af9
Guo Yunhe <i@guoyunhe.me>
2024-07-14 17:55:43 +0800

    exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,1 @@
-import cn from "classnames";
-import { CSSProperties, ReactNode } from "react";
-import "./index.css";
-
-export interface ReactLayoutProps {
-  children?: ReactNode;
-  className?: string;
-  style?: CSSProperties;
-}
-
-export function ReactLayout({ children, className, style }: ReactLayoutProps) {
-  return (
-    <ReactLayout className={cn("react-layout", className)} style={style}>
-      {children}
-    </ReactLayout>
-  );
-}
+export * from './row-layout';
